ec7e634d664b416fc14ba0a1685c592c99f7394d
Sufiyan Jamil <mohammadsufiyan474@gmail.com>
2022-05-18 14:16:12 -0700

    Github is Done

--- lib/screens.dart/home.dart
+++ lib/screens.dart/home.dart
@@ -7,6 +7,7 @@ import 'package:velocity_x/velocity_x.dart';
 // ignore: camel_case_types
 class home_page extends StatelessWidget {
   const home_page({Key? key}) : super(key: key);
+
   Widget singalProduct() {
     return Container(
       margin: EdgeInsets.symmetric(horizontal: 6),
@@ -113,11 +114,124 @@ class home_page extends StatelessWidget {
     );
   }
 
+  Widget listTile({required IconData icon, required String title}) {
+    return ListTile(
+      leading: Icon(
+        icon,
+        size: 32,
+        color: Vx.black,
+      ),
+      title: Text(
+        title,
+        style: TextStyle(color: Vx.black),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color(0xfffcbcbcb),
-      drawer: Drawer(),
+      drawer: Drawer(
+        child: Container(
+          color: Color(0xffd1ad17),
+          child: ListView(
+            children: [
+              DrawerHeader(
+                child: Row(
+                  children: [
+                    CircleAvatar(
+                      radius: 43,
+                      backgroundColor: Color(0xfffcbcbcb),
+                      child: CircleAvatar(
+                        radius: 40,
+                        backgroundColor: Vx.red600,
+                      ),
+                    ),
+                    SizedBox(
+                      width: 20,
+                    ),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        "Welcome Guest".text.size(18).bold.make(),
+                        SizedBox(
+                          height: 7,
+                        ),
+                        Container(
+                          height: 35,
+                          padding: EdgeInsets.symmetric(vertical: 2),
+                          child: SizedBox(
+                            width: 90,
+                            child: OutlinedButton(
+                              onPressed: () {},
+                              child: "Login".text.bold.black.make(),
+                              style: OutlinedButton.styleFrom(
+                                shape: StadiumBorder(),
+                                side: BorderSide(width: 2, color: Colors.black),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              listTile(
+                icon: Icons.home_outlined,
+                title: "Home",
+              ),
+              listTile(
+                icon: CupertinoIcons.cart,
+                title: "Review Cart",
+              ),
+              listTile(
+                icon: CupertinoIcons.person,
+                title: "My Profile",
+              ),
+              listTile(
+                icon: CupertinoIcons.bell,
+                title: "Notification",
+              ),
+              listTile(
+                icon: CupertinoIcons.star,
+                title: "Rate & Review",
+              ),
+              listTile(
+                icon: CupertinoIcons.heart,
+                title: "Wishlist",
+              ),
+              listTile(
+                icon: CupertinoIcons.doc,
+                title: "Raise a Complaint",
+              ),
+              listTile(
+                icon: Icons.message_outlined,
+                title: "FAQs",
+              ),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 13),
+                height: 350,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    "Contact Support".text.black.bold.make(),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    "Call us:    +923152502738".text.black.make(),
+                    SizedBox(
+                      height: 4,
+                    ),
+                    "Mail us:    hello@retry.com".text.black.make(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
       appBar: AppBar(
         iconTheme: IconThemeData(color: Colors.black),
         backgroundColor: Color(0xffd6b738),
@@ -227,7 +341,7 @@ class home_page extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 3),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
@@ -250,7 +364,7 @@ class home_page extends StatelessWidget {
               ),
             ),
             Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
+              padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 3),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
